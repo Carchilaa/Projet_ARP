dc67e304ca143c78b53795b3f844beb526bc9267
Carchilaa <caarchila2003@gmail.com>
2025-12-05 03:37:59 +0100

    Algorithme Backtrack

---
+++ code/main.py
@@ -0,0 +1,68 @@
+Hommes = ["H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8"]
+
+Femmes = ["F1", "F2", "F3", "F4",  "F5", "F6", "F7", "F8"]
+
+preferencesHommes = {
+    "H1": ["F3", "F1", "F5","F7", "F4", "F2", "F8", "F6"],
+    "H2": ["F6", "F1", "F3", "F4", "F8", "F7", "F5", "F2"],
+    "H3": ["F7", "F4", "F3", "F6", "F5", "F1", "F2", "F8"],
+    "H4": ["F5", "F3", "F8", "F2", "F6", "F1", "F4", "F7"],
+    "H5":["F4", "F1", "F2", "F8", "F7", "F3", "F6", "F5"],
+    "H6":["F6", "F2", "F5", "F7", "F8", "F4", "F3", "F1"],
+    "H7":["F7", "F8", "F1", "F6", "F2", "F3", "F4", "F5"],
+    "H8":["F2", "F6", "F7",  "F1", "F8", "F3", "F4", "F5"]
+}
+
+preferencesFemmes = {
+    "F1": ["H4", "H3", "H8", "H1", "H2", "H5", "H7", "H6"],
+    "F2": ["H3", "H7", "H5", "H8", "H6", "H4", "H1", "H2"],
+    "F3": ["H7", "H5", "H8", "H3", "H6", "H2", "H1", "H4"],
+    "F4": ["H6", "H4", "H2", "H7", "H3", "H1", "H5", "H8"],
+    "F5": ["H8", "H7", "H1", "H5", "H6", "H4", "H3", "H2"],
+    "F6": ["H5", "H4", "H7", "H6", "H2", "H8", "H3", "H1"],
+    "F7": ["H1", "H4", "H5", "H6", "H2", "H8", "H3", "H7"],
+    "F8": ["H2", "H5", "H4", "H3", "H7", "H8", "H1", "H6"]
+}
+
+#Return vrai si preferences_list[i] prefere a sur b. Faux dans le cas contraire
+def  prefers(preferences_list,a, b):
+    return preferences_list.index(a)<preferences_list.index(b)
+
+def is_stable(h, f, matching, preferencesH, preferencesF):
+    
+    for h2, f2 in matching.items():
+        
+        #On saut la comparaison du couple (h,f) avec lui meme
+        if h2 == h:
+            continue
+        
+        if prefers(preferencesF[f2], h, h2) and prefers(preferencesH[h2], f,f2):
+            return False
+        
+        if prefers(preferencesF[f], h2, h) and prefers(preferencesH[h], f2,f):
+            return False
+    return True
+
+def backtrack(i,H, F, preferencesH, preferencesF, matching, married):
+    
+    if i == len(H):
+        return matching.copy()
+    
+    h = H[i]
+    
+    for f in F:
+        if f not in married:
+            if is_stable(h,f, matching, preferencesH, preferencesF):
+                matching[h] = f
+                married.add(f)
+                
+                result = backtrack(i + 1, H, F, preferencesH, preferencesF, matching, married)
+                if result is not None:
+                    return result
+
+                married.remove(f)
+                del matching[h]
+    return None
+
+
+print(backtrack(0, Hommes, Femmes, preferencesHommes, preferencesFemmes,{}, set()))
